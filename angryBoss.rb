a849ea51e007f7b94d2f71f01252b67431637303
Taylor Swanson <hisankitsune@gmail.com>
2015-12-15 11:46:54 -0500

    Create angryBoss.rb

---
+++ angryBoss.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/env ruby
+
+# Stupid employee you made me look bad, BOOGA BOOGA BOOGA!
+puts 'Hello what can I do for you?' + 'Sorry your not important whats your name again?'
+name = gets.chomp
+puts 'Ok so ' + name + ' what do you want, Im a busy boss?'
+want = gets.chomp
+puts 'YOU WANT WHAT?!?! A "' + want.upcase.to_s + '"?! ' + name.upcase + ' YOUR FIRED!!'
